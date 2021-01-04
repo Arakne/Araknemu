b38b84c4262da1f92166b98844f04fae3d57d9b4
Javdu10 <jav.valentin@gmail.com>
2021-01-04 18:17:04 +0100

    revert

--- src/main/java/fr/quatrevieux/araknemu/game/listener/player/SavePlayer.java
+++ src/main/java/fr/quatrevieux/araknemu/game/listener/player/SavePlayer.java
@@ -35,7 +35,6 @@ final public class SavePlayer implements Listener<Disconnected> {
 
     @Override
     public void on(Disconnected event) {
-        player.properties().life().stopLifeRegeneration();
         player.save();
     }
 
