b20826e9847d84ceac83cdbd2fb35aa6277541aa
Javdu10 <jav.valentin@gmail.com>
2021-01-21 19:44:07 +0100

    implement fighter

--- src/main/java/fr/quatrevieux/araknemu/game/fight/fighter/monster/InvocationFighter.java
+++ src/main/java/fr/quatrevieux/araknemu/game/fight/fighter/monster/InvocationFighter.java
@@ -2,18 +2,23 @@ package fr.quatrevieux.araknemu.game.fight.fighter.monster;
 
 import java.util.Optional;
 
+import fr.arakne.utils.maps.constant.Direction;
+import fr.quatrevieux.araknemu.game.fight.Fight;
+import fr.quatrevieux.araknemu.game.fight.castable.effect.buff.Buffs;
 import fr.quatrevieux.araknemu.game.fight.castable.weapon.CastableWeapon;
-import fr.quatrevieux.araknemu.game.fight.fighter.AbstractFighter;
 import fr.quatrevieux.araknemu.game.fight.fighter.Fighter;
 import fr.quatrevieux.araknemu.game.fight.fighter.FighterCharacteristics;
 import fr.quatrevieux.araknemu.game.fight.fighter.FighterLife;
 import fr.quatrevieux.araknemu.game.fight.fighter.PassiveFighter;
+import fr.quatrevieux.araknemu.game.fight.fighter.States;
 import fr.quatrevieux.araknemu.game.fight.fighter.operation.FighterOperation;
+import fr.quatrevieux.araknemu.game.fight.map.FightCell;
 import fr.quatrevieux.araknemu.game.fight.team.FightTeam;
+import fr.quatrevieux.araknemu.game.fight.turn.FightTurn;
 import fr.quatrevieux.araknemu.game.spell.SpellList;
 import fr.quatrevieux.araknemu.game.world.creature.Sprite;
 
-final public class InvocationFighter extends AbstractFighter {
+final public class InvocationFighter implements Fighter {
     final private Fighter fighter;
     final private PassiveFighter invoker;
 
@@ -75,5 +80,80 @@ final public class InvocationFighter extends AbstractFighter {
     @Override
     public Optional<PassiveFighter> invoker() {
         return Optional.of(invoker);
+    }
+
+    @Override
+    public void attach(Object key, Object value) {
+        fighter.attach(key, value);
+    }
+
+    @Override
+    public Fight fight() {
+        return fighter.fight();
+    }
+
+    @Override
+    public void init() {
+        fighter.init();
+    }
+
+    @Override
+    public boolean isOnFight() {
+        return fighter.isOnFight();
+    }
+
+    @Override
+    public void joinFight(Fight fight, FightCell startCell) {
+        fighter.joinFight(fight, startCell);
+    }
+
+    @Override
+    public void play(FightTurn turn) {
+        fighter.play(turn);
+    }
+
+    @Override
+    public void setOrientation(Direction orientation) {
+        fighter.setOrientation(orientation);
+    }
+
+    @Override
+    public void stop() {
+        fighter.stop();
+    }
+
+    @Override
+    public FightCell cell() {
+        return fighter.cell();
+    }
+
+    @Override
+    public Direction orientation() {
+        return fighter.orientation();
+    }
+
+    @Override
+    public void dispatch(Object event) {
+        fighter.dispatch(event);
+    }
+
+    @Override
+    public Object attachment(Object key) {
+        return fighter.attachment(key);
+    }
+
+    @Override
+    public Buffs buffs() {
+        return fighter.buffs();
+    }
+
+    @Override
+    public void move(FightCell cell) {
+        fighter.move(cell);
+    }
+
+    @Override
+    public States states() {
+        return fighter.states();
     }
 }
