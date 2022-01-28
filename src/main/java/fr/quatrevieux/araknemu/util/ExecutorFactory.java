7075805fd0a36e021b044bd8676128dd135875a4
vincent4vx <quatrevieux.vincent@gmail.com>
2022-01-28 18:45:22 +0100

    [ci] fix build

--- src/main/java/fr/quatrevieux/araknemu/util/ExecutorFactory.java
+++ src/main/java/fr/quatrevieux/araknemu/util/ExecutorFactory.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Araknemu.
+ *
+ * Araknemu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Araknemu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Araknemu.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2017-2022 Vincent Quatrevieux
+ */
+
 package fr.quatrevieux.araknemu.util;
 
 import java.util.ArrayList;
