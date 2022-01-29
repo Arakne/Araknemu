74edc928049bef17ee9f5ce482505049ceb4df6d
vincent4vx <quatrevieux.vincent@gmail.com>
2022-01-29 17:04:45 +0100

    DO NOT MERGE: testing codecov PR check

---
+++ src/test/java/fr/quatrevieux/araknemu/game/NotCoveredServiceTest.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of Araknemu.
+ *
+ * Araknemu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Araknemu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Araknemu.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2017-2022 Vincent Quatrevieux
+ */
+
+package fr.quatrevieux.araknemu.game;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotCoveredServiceTest {
+    @Test
+    void foo() {
+        assertEquals("bar", new NotCoveredService().foo());
+    }
+
+    @Test
+    void display() {
+        PrintStream last = System.out;
+
+        ByteOutputStream stream = new ByteOutputStream();
+        System.setOut(new PrintStream(stream));
+
+        new NotCoveredService().display();
+
+        assertEquals("bar\n", stream.toString());
+
+        System.setOut(last);
+    }
+}
